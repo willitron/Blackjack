a69b82910b265fe6736c20334c8c91e86ccafa4b
willtron <wilsony0007@gmail.com>
2025-08-16 10:51:04 -0400

    ultimaMod3

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,15 @@ export default defineConfig({
     base : '/Blackjack/',
     plugins : []
 });
+
+export const drawCard = (card, container) => {
+
+    const image = document.createElement("img");
+  
+    image.src = `/Blackjack/assets/cards/${card}.png`;
+  
+    image.classList.add("carta");
+  
+    container.appendChild(image);
+  
+  };
